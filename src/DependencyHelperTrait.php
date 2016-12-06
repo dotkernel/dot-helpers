67f1f009435a303d202e64ae7797a0bf553a50ff
n3vrax <n3vrax@gmail.com>
2016-12-06 19:43:12 +0200

    added a dependency helper trait to fetch deps from container or instantiate them, depending on availability

---
+++ src/DependencyHelperTrait.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright: DotKernel
+ * @library: dotkernel/dot-helpers
+ * @author: n3vrax
+ * Date: 12/6/2016
+ * Time: 7:39 PM
+ */
+
+namespace Dot\Helpers;
+
+
+use Interop\Container\ContainerInterface;
+
+class DependencyHelperTrait
+{
+    public function getDependencyObject(ContainerInterface $container, $name)
+    {
+        $dependency = $name;
+        if($container->has($dependency)) {
+            $dependency = $container->get($dependency);
+        }
+
+        if(is_string($dependency) && class_exists($dependency)) {
+            $dependency = new $dependency;
+        }
+
+        return $dependency;
+    }
+}
